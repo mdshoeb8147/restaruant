462d48011d8c000ee58c848e1c391584155f9633
mdshoeb8147 <mdshoeb8147@gmail.com>
2024-01-27 12:54:14 +0530

    issue resolved

--- Backend/controller/reservation.js
+++ Backend/controller/reservation.js
@@ -3,7 +3,7 @@ import { Reservation } from "../models/reservationSchema.js";
 
 export const sendReservation = async (req, res, next) => {
   const { firstName, lastName, email, phone, date, time } = req.body;
-  if (firstName || lastName || email || phone || date || time) {
+  if (!firstName || !lastName || !email || !phone || !date || !time) {
     return next(new ErrorHandler("please fill full details!", 400));
   }
   try {
